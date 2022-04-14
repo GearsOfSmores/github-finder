c35ad3fee4dbc4c74535215c10a150f692ab580f
Matt Sutton <msutton8822@gmail.com>
2022-04-14 14:27:04 -0400

    Home page, about, not found layout

---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+
+
+function About() {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>This app allows users to search for Github proflies and see the details of each profile</p>
+    </div>
+  )
+}
+
+export default About
